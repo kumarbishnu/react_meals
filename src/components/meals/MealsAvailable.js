ffdb1360fb5908f5bdd2bb89956d26d754f6d4b3
Bishnu Kumar Das <kumarbishnu202@gmail.com>
2021-06-20 21:43:14 +0545

    Fetch meals from backend

--- src/components/meals/MealsAvailable.js
+++ src/components/meals/MealsAvailable.js
@@ -1,17 +1,53 @@
 import Card from "../ui/Card";
 import MealItem from "./MealItem";
-
-const DUMMY_MEALS = [
-    {id: 'm1', name: 'Sushi', description: 'Finest fish and veggies', price: 22.99},
-    {id: 'm2', name: 'Schnitzel', description: 'A german specialty!', price: 16.5},
-    {id: 'm3', name: 'Barbeque Burger', description: 'America, Raw, Rusty', price: 12.99},
-    {id: 'm4', name: 'Green Bowl', description: 'Healthy...and green...', price: 18.99},
-]
+import {useEffect, useState} from "react";
 
 const MealsAvailable = () => {
+
+    const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            setLoading(true);
+
+            const response = await fetch('https://mero-khata-d9bbc-default-rtdb.firebaseio.com/meals.json');
+            if (!response.ok) {throw new Error('Something went wrong!');}
+
+            const responseData = await response.json();
+            const meals = [];
+            for (const key in responseData) {
+                meals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                })
+            }
+            setMeals(meals);
+            setLoading(false);
+        }
+
+        fetchMeals().catch(error => {
+            setLoading(false);
+            setError(error.message);
+        });
+    }, []);
+
+    if (error) {
+        return <div className="alert alert-danger w-75 mx-auto">{error}</div>
+    }
+
+    if (loading) {
+        return <Card className="w-75 mx-auto">
+            <div className="spinner-border text-danger my-5 mx-auto" />
+        </Card>
+    }
+
     return <Card className="text-dark w-75 mx-auto">
         <ul className="list-group list-group-flush">
-        {DUMMY_MEALS.map(meal => <MealItem key={meal.id} meal={meal} />)}
+        {meals.map(meal => <MealItem key={meal.id} meal={meal} />)}
         </ul>
     </Card>
 }
